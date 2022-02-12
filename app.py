03b2f573cf359a1573f9bbb2f60a0e8862272587
Paul <59670248+impaulb@users.noreply.github.com>
2022-02-12 13:57:51 -0800

    Correct implementation

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 from flask import Flask
+from flask import request
+import redis
+import json
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    file = open("env.txt","r")
+    env_vars = file.readlines()
+    
+    r = redis.Redis(
+        host = env_vars[0][:-1],
+        port = int(env_vars[1][:-1]),
+        password = env_vars[2]
+    )
+    
+    file.close()
+    
+    try:
+        id = request.args.get('id')
+        data = r.get(id)
+        data_json = json.loads(data)
+        return data_json
+    except:
+        error_result = {'depression_stats':99999,"student_to_mh_ratio":99999, "investment_into_mh":99999,"cost_of_living_and_debt":99999,"suicide_rate":99999}
+        return json.dumps(error_result)
