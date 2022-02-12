5b8edcba178d7b2cf91b2daef8a437b02e0eb3dc
Paul <59670248+impaulb@users.noreply.github.com>
2022-02-12 12:40:51 -0800

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,10 +6,6 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -24,24 +20,8 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
-    
     @app.route('/data')
     def data():
-        int depression_rate
-        int suicide_rate
-
-    @app.route('/colleges')
-    def colleges(collegelist):
-        str collegeID = ("")
-        return "List of colleges is:" + collegelist
-
-    @app.route('/investment')
-    def investment():
-        int investment_amount
+        return '0'
         
+    return app
